001b874f234524621cf7da669aeecdd4579013d3
anushabhimala <anushapriyabhimala@gmail.com>
2016-11-16 14:42:11 -0800

    Display Menu assignment

--- ProgramCSharp/Anusha.Assignment2/Program.cs
+++ ProgramCSharp/Anusha.Assignment2/Program.cs
@@ -10,63 +10,104 @@ namespace Anusha.Assignment2
     {
         static void Main(string[] args)
         {
-            string[] arrayOfStrings = new string[5];
-
-            int totalNumOfPalindromes = 0;
-            string stringOfPalindromes = null;
-            int count = 0;
-
-            //string palindromeString = null;
-            for (int i = 0; i < arrayOfStrings.Length; i++)
+            bool isExit = false;
+            while (isExit == false)
             {
-                Console.WriteLine("Please enter {0}th element of arrayOfStrings", i);
-                arrayOfStrings[i] = Console.ReadLine();
-            }
-            foreach (string word in arrayOfStrings)
-            {
-                Console.WriteLine(word);
+                isExit = DisplayMenu();
             }
 
-            for (int j = 0; j < arrayOfStrings.Length; j++)
+            
+            
+        }
+
+        private static bool DisplayMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose one option from given below");
+            Console.WriteLine("1. Print table of a given number");
+            Console.WriteLine("2. Print reverse of a number");
+            Console.WriteLine("3. Print cumulative Characters for a given string");
+            Console.WriteLine("4. Exit");
+           string userInput = Console.ReadLine();
+            if(userInput == "1")
             {
-                var modifiedString = arrayOfStrings[j];
-                modifiedString = modifiedString.Replace(" ", "");
-                arrayOfStrings[j] = modifiedString;
-
-                char[] stringCharArray = arrayOfStrings[j].ToCharArray();
-                for (int k = 0; k < stringCharArray.Length; k++)
-                {
-                    count = 0;
-
-                    if (stringCharArray[k] == stringCharArray[(((stringCharArray.Length) - 1) - k)])
-                    {
-
-                        count = count + 1;
-                       
-                    }
-                    else
-                    {
-                        count = 0;
-                        break;
-
-                    }
-
-                }
-                if (count > 0)
-                {
-                    totalNumOfPalindromes = totalNumOfPalindromes + 1;
-                    stringOfPalindromes = arrayOfStrings[j] + "," + stringOfPalindromes;
-                }
-
+                PrintingTable();
+                return false;
             }
+            else if(userInput == "2")
+            {
+                ReversingNumber();
+                return false;
+            }
+            else if(userInput == "3")
+            {
+                CumulativeCharactersOfString();
+                return false;
+            }
+            else if(userInput == "4")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
+        private static void CumulativeCharactersOfString()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter a string");
+            string inputString = Console.ReadLine();
+            string outputString = "";
+            int i = 0;
+            do
+            {
+                outputString = outputString + inputString[i];
+                Console.WriteLine(outputString);
+                i++;
 
-            Console.WriteLine("The number of Palindrome strings are {0}", totalNumOfPalindromes);
-            Console.WriteLine(stringOfPalindromes);
+            } while (i < inputString.Length);
+            Console.ReadLine();
+           
+        }
+
+        private static void ReversingNumber()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter any number");
+            int inputNumber = int.Parse(Console.ReadLine());
+            double outputNumber = 0;
+            int remainder = 0 ;
+            
+         
+           //outputNumber = Math.Pow(10, i);
+           do
+            {
+               remainder = inputNumber %  10;
+               outputNumber = outputNumber * 10 + remainder;
+               
+                inputNumber = inputNumber / 10;
+            } while (inputNumber != 0) ;
+
+                Console.WriteLine(outputNumber);
             Console.ReadLine();
 
-            
-            
+        }
+
+        private static void PrintingTable()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter any number");
+            int inputNumber = int.Parse(Console.ReadLine());
+            int i = 0;
+           while( i<=10)
+            {
+                int outputNumber = inputNumber * i;
+                Console.WriteLine("{0} * {1} = {2}", inputNumber, i, outputNumber);
+                i++;
+            }
+            Console.ReadLine();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
         }
     }
 }
